6129dcf73051c331c388346d24eea9ade3311aca
Marina <moya_poshta@list.ru>
2024-11-17 21:44:57 +0300

    Finish 2.0

---
+++ user_links_api/apps/links/services/headers.py
@@ -0,0 +1,16 @@
+headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+            'Accept-Language': 'ru,en-US;q=0.7,en;q=0.3',
+            # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Connection': 'keep-alive',
+            # 'Cookie': '_ga_5LK9Y0PNLQ=GS1.1.1731794718.8.0.1731794718.60.0.0; _ga=GA1.1.1728279502.1728934335; _ym_uid=169384474267578418; _ym_d=1728934336; sessionid=eaz4v8pekewjiuklquj4enjgzy5chjfy; csrftoken=g3JhO77QcBIaXDswePm7yf7yFhbxbQg7fyOl7bTUVz46D2Ab32yWXGABCdG9gJWX; _ym_isad=2; _ym_visorc=w',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'Priority': 'u=0, i',
+            # Requests doesn't support trailers
+            # 'TE': 'trailers',
+        }
--- user_links_api/apps/links/views/create_link_view.py
+++ user_links_api/apps/links/views/create_link_view.py
@@ -1,6 +1,8 @@
 from ..serializers import LinksSerializer
 from ..models import LinksModel
 from .parser_view import xpath
+from ..services.headers import headers
+from ..services.cookies import cookies
 
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
@@ -16,54 +18,15 @@ class LinkCreateView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self,request):
-        cookies = {
-        '_ga_5LK9Y0PNLQ': 'GS1.1.1731794718.8.0.1731794718.60.0.0',
-        '_ga': 'GA1.1.1728279502.1728934335',
-        '_ym_uid': '169384474267578418',
-        '_ym_d': '1728934336',
-        'sessionid': 'eaz4v8pekewjiuklquj4enjgzy5chjfy',
-        'csrftoken': 'g3JhO77QcBIaXDswePm7yf7yFhbxbQg7fyOl7bTUVz46D2Ab32yWXGABCdG9gJWX',
-        '_ym_isad': '2',
-        '_ym_visorc': 'w',
-        'ps_l': '1',
-        'ps_n': '1',
-        'mid': 'Zq9BaAAEAAELVEwsoL3EovszmcLZ',
-        'datr': 'Z0GvZr0dT1zjDZEDPpK8xdB3',
-        'ig_did': '4D0A7D2B-2A33-47DA-A941-D99376C803C5',
-        'ig_nrcb': '1',
-        'csrftoken': 't7pHgrErNIBIyi67qsTsIyaaCKzkhTYY',
-        'ds_user_id': '4492254664',
-        'sessionid': '4492254664%3AelPXos7AnVHWvr%3A7%3AAYdJvpRyl1slhHH55kgTawswNof6QWSA5Y1XT418gpk',
-        'wd': '1920x431',
-        'ig_direct_region_hint': '"PRN\\0544492254664\\0541763332619:01f7e56ad77bcc523c3ff6360f5287487231e1be83e028ef4fb749778de23e767518ab2f"',
-        'rur': '"CLN\\0544492254664\\0541763401394:01f7caccc48a7cb92aab35aac80c529f5413c7a675a5767c71975e58bfed6d5f0a13540b"',
-    }
-
-
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
-            'Accept-Language': 'ru,en-US;q=0.7,en;q=0.3',
-            # 'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Connection': 'keep-alive',
-            # 'Cookie': '_ga_5LK9Y0PNLQ=GS1.1.1731794718.8.0.1731794718.60.0.0; _ga=GA1.1.1728279502.1728934335; _ym_uid=169384474267578418; _ym_d=1728934336; sessionid=eaz4v8pekewjiuklquj4enjgzy5chjfy; csrftoken=g3JhO77QcBIaXDswePm7yf7yFhbxbQg7fyOl7bTUVz46D2Ab32yWXGABCdG9gJWX; _ym_isad=2; _ym_visorc=w',
-            'Upgrade-Insecure-Requests': '1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-User': '?1',
-            'Priority': 'u=0, i',
-            # Requests doesn't support trailers
-            # 'TE': 'trailers',
-        }
-
-
         try:
+            owner = request.user.id
             url = request.POST['url']
+
             try:
                 page = requests.get(url,cookies=cookies, headers=headers).content.decode('UTF-8')
             except:
                 page = requests.get(url,cookies=cookies, headers=headers).text
+
             title = xpath(page, '//meta[@property="og:title"]//@content').extract_first()
             if title == '' or not title:
                 title = xpath(page, '//head/title//text()').extract_first()
@@ -105,8 +68,7 @@ class LinkCreateView(APIView):
                         kind_link = 'WEBSITE'
             else:
                 kind_link = 'WEBSITE'
-          
-            owner = request.user.id
+                      
             types_image_example = ['bmp', 'dib', 'gif', 'jfif', 'jpe', 'jpg', 'jpeg', 'pbm', 'pgm', 'ppm', 'pnm', 'pfm', 'png', 'apng', 'blp', 
                            'bufr', 'cur', 'pcx', 'dcx', 'dds', 'ps', 'eps', 'fit', 'fits', 'fli', 'flc', 'ftc', 'ftu', 'gbr', 'grib', 
                            'h5', 'hdf', 'jp2', 'j2k', 'jpc', 'jpf', 'jpx', 'j2c', 'icns', 'ico', 'im', 'iim', 'mpg', 'mpeg', 'tif', 
@@ -135,7 +97,7 @@ class LinkCreateView(APIView):
             if len(LinksModel.objects.filter(url_page=url_page).filter(owner=owner))>0:
                 status = HTTP_400_BAD_REQUEST
                 return Response ({"Status":"Link Already Used"},status=status)
-            # import pdb; pdb.set_trace()
+
             serializer = LinksSerializer(data=data)
             serializer.is_valid(raise_exception=True)
 
